e8280735e7db57bf1f4970d40ce5218782b72e18
武井真輝 <masaki@mba.local>
2021-09-25 20:01:44 +0900

    Add ido_keido function

---
+++ ido_keido.js
@@ -0,0 +1,113 @@
+// 緯度（度）：北緯 0～90、南緯 -0～-90
+// 南緯ｉ度⇔－（北緯）ｉ度
+
+// 経度（度）：東経 0～180、西経 0～-180
+// 西経ｋ度⇔－（東経）ｋ度
+
+const ido = (i0, k0, d, a) => {
+  // i0：出発点緯度（度）
+  // k0：出発点経度（度）
+  // d：目標点距離（ｍ）
+  // a：目標点方位（度）
+  // ido：出発点から距離d（ｍ）、方位a（度）地点の緯度（度）
+
+  const pi = Math.PI;
+  const rx = 6378137; // 赤道半径(m)
+  const e2 = 6.69437999019758E-03 // 第2離心率(e^2)
+
+  const wt = Math.sqrt(1 - e2 * Math.pow(Math.sin(i0 * pi / 180), 2)); // 仮のW（第１近似）
+  const mt = rx * (1 - e2) / Math.pow(wt, 3); // 仮のM（第１近似）
+  const diT = d * Math.cos(a * pi / 180) / mt; // 仮のdi（第１近似）
+  const i = i0 * pi / 180 + diT / 2;
+  
+  const w = Math.sqrt(1 - e2 * Math.pow(Math.sin(i), 2));
+  const m = rx * (1 - e2) / Math.pow(w, 3);
+  const di = d * Math.cos(a * pi / 180) / m;
+  const ido = i0 + di * 180 / pi;
+  return ido;
+}
+
+const keido = (i0, k0, d, a) => {
+  // i0：出発点緯度（度）
+  // k0：出発点経度（度）
+  // d：目標点距離（ｍ）
+  // a：目標点方位（度）
+  // keido：出発点から距離d（ｍ）、方位a（度）地点の経度（度）
+
+  const pi = Math.PI;
+  const rx = 6378137; // 赤道半径(m)
+  const e2 = 6.69437999019758E-03 // 第2離心率(e^2)
+
+  const wt = Math.sqrt(1 - e2 * Math.pow(Math.sin(i0 * pi / 180), 2)); // 仮のW（第１近似）
+  const mt = rx * (1 - e2) / Math.pow(wt, 3); // 仮のM（第１近似）
+  const diT = d * Math.cos(a * pi / 180) / mt; // 仮のdi（第１近似）
+  const i = i0 * pi / 180 + diT / 2;
+
+  const w = Math.sqrt(1 - e2 * Math.pow(Math.sin(i), 2));
+  const n = rx / w;
+  const dk = d * Math.sin(a * pi / 180) / (n * Math.cos(i));
+  const keido = k0 + dk * 180 / pi;
+  return keido;
+}
+
+const geoDistance = (lat1, lng1, lat2, lng2, precision) => {
+  // 引数 precision は小数点以下の桁数（距離の精度）
+  var distance = 0;
+  if ((Math.abs(lat1 - lat2) < 0.00001) && (Math.abs(lng1 - lng2) < 0.00001)) {
+    distance = 0;
+  } else {
+    lat1 = lat1 * Math.PI / 180;
+    lng1 = lng1 * Math.PI / 180;
+    lat2 = lat2 * Math.PI / 180;
+    lng2 = lng2 * Math.PI / 180;
+    
+    var A = 6378140;
+    var B = 6356755;
+    var F = (A - B) / A;
+    
+    var P1 = Math.atan((B / A) * Math.tan(lat1));
+    var P2 = Math.atan((B / A) * Math.tan(lat2));
+    
+    var X = Math.acos(Math.sin(P1) * Math.sin(P2) + Math.cos(P1) * Math.cos(P2) * Math.cos(lng1 - lng2));
+    var L = (F / 8) * ((Math.sin(X) - X) * Math.pow((Math.sin(P1) + Math.sin(P2)), 2) / Math.pow(Math.cos(X / 2), 2) - (Math.sin(X) - X) * Math.pow(Math.sin(P1) - Math.sin(P2), 2) / Math.pow(Math.sin(X), 2));
+    
+    distance = A * (X + L);
+    var decimal_no = Math.pow(10, precision);
+    distance = Math.round(decimal_no * distance / 1) / decimal_no;   // kmに変換するときは(1000で割る)
+  }
+  return distance;
+}
+
+const geoDirection = (lat1, lng1, lat2, lng2) => {
+  // 緯度経度 lat1, lng1 の点を出発として、緯度経度 lat2, lng2 への方位
+  // 北を０度で右回りの角度０～３６０度
+  var Y = Math.cos(lng2 * Math.PI / 180) * Math.sin(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
+  var X = Math.cos(lng1 * Math.PI / 180) * Math.sin(lng2 * Math.PI / 180) - Math.sin(lng1 * Math.PI / 180) * Math.cos(lng2 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
+  var dirE0 = 180 * Math.atan2(Y, X) / Math.PI; // 東向きが０度の方向
+  if (dirE0 < 0) {
+    dirE0 = dirE0 + 360; //0～360 にする。
+  }
+  var dirN0 = (dirE0 + 90) % 360; //(dirE0+90)÷360の余りを出力 北向きが０度の方向
+  return dirN0;
+}
+ 
+const calcIdoKeido = (i0, k0, i1, k1, d) => {
+  const direction = geoDirection(i0, k0, i1, k1);
+  const a0 = direction - 90;
+  if (a0 < 0) {
+    a0 += 360;
+  }
+  const a1 = direction + 90;
+  if (a1 >= 360) {
+    a1 -= 360;
+  }
+  const x1 = ido(i0, k0, d, a0);
+  const y1 = keido(i0, k0, d, a0);
+  const x2 = ido(i0, k0, d, a1);
+  const y2 = keido(i0, k0, d, a1);
+  return [x1, y1, x2, y2];
+}
+
+module.exports = {
+  calcIdoKeido,
+}
--- template.js
+++ template.js
@@ -4,13 +4,16 @@ const dotenv = require('dotenv');
 const admin = require('firebase-admin');
 // const functions = require('firebase-functions');
 const ServiceAccount = require('./ServiceAccount.json');
+const { calcIdoKeido } = require('./ido_keido');
 
 // Firebaseのinitialize
 admin.initializeApp({ credential: admin.credential.cert(ServiceAccount) });
 
 // Firestoreの参照を取得
 const db = admin.firestore();
+const docRef_users = db.collection('users')
 const docRef_shops = db.collection('shops')
+const docRef_yrmcs = db.collection('yrmcs')
 // const docRef_dests = db.collection('dests')
 
 dotenv.config();
@@ -41,15 +44,20 @@ const setDest = async (req, res) => {
     const dest = {
         currentLoc: req.body.currentLoc,  // {X: 100, Y:100}
         destination: req.body.destination, // {X: 10, Y:10}
-        timestamp: admin.firestore.FieldValue.serverTimestamp(), // このように書くとtimestampも保存できる
+        // timestamp: admin.firestore.FieldValue.serverTimestamp(), // このように書くとtimestampも保存できる
     };
     // await docRef_dests.add(dest)
 
     // calc 
-    const x1 = 30;
-    const y1 = 80;
-    const x2 = 100;
-    const y2 = 100;
+    const i0 = dest.currentLoc.X;
+    const k0 = dest.currentLoc.Y;
+    const i1 = dest.destination.X;
+    const k1 = dest.destination.Y;
+    const xy = calcIdoKeido(i0, k0, i1, k1, thDistance);
+    const x1 = Math.min(xy[0], xy[2]);
+    const y1 = Math.min(xy[1], xy[3]);
+    const x2 = Math.max(xy[0], xy[2]);
+    const y2 = Math.max(xy[1], xy[3]);
 
     const snapshots = await docRef_shops.where('coordinate.X', '>=', x1).get();
     // const snapshots = await docRef_shops.where('coordinate.X', '>=', x1).where('coordinate.X', '<=', x2).where('coordinate.Y', '>=', y1).where('coordinate.Y', '<=', y2).get();
@@ -67,13 +75,13 @@ const setDest = async (req, res) => {
 }
 
 const setYrmc = (req, res) => {
-    
-    //ここにDB処理
-
-    res.send("true");
+    const yrmc = {
+        yrmcList: req.body.yrmcList,
+        user_id: '',
+    }
+    await docRef_yrmcs.add(yrmc);
+    res.send("success!!!")
 }
-
-
 
 module.exports = {
     testTemplate,
